9d7d15259b9b918b8fbc024d41faa56817740c57
Ernestas Poskus <ernestas.poskus@gmail.com>
2019-02-18 16:17:25 +0200

    Add command_run_once to base artifact

--- libraries/base_artifact.rb
+++ libraries/base_artifact.rb
@@ -8,6 +8,7 @@ class Chef
       attribute(:command_creates, kind_of: String, default: '')
       attribute(:command_ignore_failure, kind_of: [TrueClass, FalseClass], default: false)
       attribute(:command_returns, kind_of: [Integer, Array], default: 0)
+      attribute(:command_run_once, kind_of: [TrueClass, FalseClass], default: true)
     end
   end
 
@@ -19,6 +20,19 @@ class Chef
       def execute_command
         env = vitess_environment
         cmd = "#{bin_location} -alsologtostderr=1 #{new_resource.command}"
+        run_once_cache_file = ::File.join(
+          Chef::Config[:file_cache_path],
+          "vitess_run_once_#{Digest::MD5.hexdigest(new_resource.command)}"
+        )
+
+        file run_once_cache_file do
+          content "RUN ONCE FILE for: #{new_resource.name}, cmd: #{cmd}"
+          mode '0755'
+          owner 'root'
+          group 'root'
+          action :nothing
+        end
+
         execute "executing command: #{new_resource.bin_name} #{new_resource.command}" do
           environment env
           command cmd
@@ -28,6 +42,8 @@ class Chef
           group new_resource.group
           ignore_failure new_resource.command_ignore_failure
           returns new_resource.command_returns
+          notifies :create_if_missing, "file[#{run_once_cache_file}]", :immediately
+          not_if { ::File.exist?(run_once_cache_file) } if new_resource.command_run_once
         end
       end
     end
