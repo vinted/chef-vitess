5b53f6da5b3a25e67e22fc015e9df94c4bd20b3b
Ernestas Poskus <ernestas.poskus@gmail.com>
2019-02-18 10:58:25 +0200

    Update vtctld webdir locations

--- libraries/vtctld_service.rb
+++ libraries/vtctld_service.rb
@@ -16,41 +16,40 @@ class Chef
       attribute(:args, kind_of: Hash, default: lazy { node['vitess']['vtctld'] })
       attribute(:web_dir_cookbook, kind_of: String, default: 'vitess')
       attribute(:web_dir2_cookbook, kind_of: String, default: 'vitess')
-
-      def additional_args
-        args = {}
-        args['web_dir'] = vtctld_web_vtctld if install_web_dir?
-        args['web_dir2'] = vtctld_web_vtctld2 if install_web_dir2?
-        args
-      end
-
-      def install_web_dir?
-        node['vitess']['vtctld']['web_dir'].nil?
-      end
-
-      def install_web_dir2?
-        node['vitess']['vtctld']['web_dir2'].nil?
-      end
-
-      def vtctld_web_vtctld
-        @vtctld_web_vtctld ||= ::File.join(vtctld_web_dir, 'vtctld2')
-      end
-
-      def vtctld_web_vtctld2
-        @vtctld_web_vtctld2 ||= ::File.join(vtctld_web_dir, 'vtctld')
-      end
-
-      def vtctld_web_dir
-        @vtctld_web_dir ||= ::File.join(vtroot, 'web')
-      end
     end
   end
 
   class Provider
     # Vtctld service installation and configuration
     class VtctldService < VitessBaseService
-
       provides(:vtctld_service)
+
+      def additional_args
+        args = {}
+        args['web_dir'] = vtctld_web_vtctld if install_web_dir?
+        args['web_dir2'] = "#{vtctld_web_vtctld2}/app" if install_web_dir2?
+        args
+      end
+
+      def install_web_dir?
+        new_resource.args['web_dir'].nil?
+      end
+
+      def install_web_dir2?
+        new_resource.args['web_dir2'].nil?
+      end
+
+      def vtctld_web_vtctld
+        @vtctld_web_vtctld ||= ::File.join(vtctld_web_dir, 'vtctld')
+      end
+
+      def vtctld_web_vtctld2
+        @vtctld_web_vtctld2 ||= ::File.join(vtctld_web_dir, 'vtctld2')
+      end
+
+      def vtctld_web_dir
+        @vtctld_web_dir ||= ::File.join(new_resource.vtroot, 'web')
+      end
 
       def action_delete
         service new_resource.service_name do
@@ -61,12 +60,10 @@ class Chef
       protected
 
       def install_web_directory
-        return unless new_resource.install_web_dir? || new_resource.install_web_dir2?
-        create_directories [
-          new_resource.vtctld_web_dir,
-        ]
+        return unless install_web_dir? || new_resource.install_web_dir2?
+        create_directories [vtctld_web_dir]
 
-        remote_directory new_resource.vtctld_web_vtctld2 do
+        remote_directory vtctld_web_vtctld2 do
           cookbook new_resource.web_dir_cookbook
           source 'web/vtctld'
           owner new_resource.user
@@ -78,7 +75,7 @@ class Chef
           action :create
         end
 
-        remote_directory new_resource.vtctld_web_vtctld do
+        remote_directory vtctld_web_vtctld do
           cookbook new_resource.web_dir2_cookbook
           source 'web/vtctld2'
           owner new_resource.user
