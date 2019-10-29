1e6bf584e58e74dc13696114a8a25a51c5430a6b
Tomas Didžiokas <tomas@vinted.com>
2019-10-29 18:46:17 +0200

    Remove .generate

--- .generate/normalize.rb
+++
@@ -1,9 +0,0 @@
-stdin = STDIN.read
-
-stdin.lines.each_slice(2) do |line|
-  key, comment = line.map(&:strip).join(' ').split(' ', 2)
-  key = key.sub('-', '')
-  next if key =~ /^test/
-  next if key =~ /^version/
-  puts "#{key} #{comment}"
-end
