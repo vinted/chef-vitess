301b3bfe3d1558f9f8c6ad94d00f0a40ce488ac5
vokockis <228085+DeathBorn@users.noreply.github.com>
2020-06-12 10:17:45 +0300

    add Vitess Release 6.0-afe46d8. remove old 6.0 releases

--- attributes/releases.rb
+++ attributes/releases.rb
@@ -1,17 +1,5 @@
-# 20-05-01 at 09:59:07 PM UTC
-default['vitess']['releases']['9fe7fd3']['url'] =
-  'https://github.com/planetscale/vitess-releases/releases/download/9fe7fd3/vitess-6.0-9fe7fd3.tar.gz'
-default['vitess']['releases']['9fe7fd3']['checksum'] =
-  'c69fba47b364b4eea75b0bff478777764b627dbcd836f723397f306673fe269b'
-
-# 2020-05-14 at 08:44:13 PM UTC
-default['vitess']['releases']['25db221']['url'] =
-  'https://github.com/planetscale/vitess-releases/releases/download/25db221/vitess-6.0-25db221.tar.gz'
-default['vitess']['releases']['25db221']['checksum'] =
-  'b8c1ac3b7f9ed41d544a22cb63bfd213e1e5812f6d427fb16c8c485b6ad13953'
-
-# 2020-06-04 at 12:01:01 AM UTC
-default['vitess']['releases']['41c356e']['url'] =
-  'https://github.com/planetscale/vitess-releases/releases/download/41c356e/vitess-6.0-41c356e.tar.gz'
-default['vitess']['releases']['41c356e']['checksum'] =
-  '3bda4eeb3237b1f73b6c4e207372ec8aec9a855ec640f7300c172d20bca382b9'
+# 2020-06-10 at 11:45:51 PM UTC
+default['vitess']['releases']['afe46d8']['url'] =
+  'https://github.com/planetscale/vitess-releases/releases/download/afe46d8/vitess-6.0-afe46d8.tar.gz'
+default['vitess']['releases']['afe46d8']['checksum'] =
+  '01f996ce81d0f17768de4fa8c2405c0625312a5b4abc0940d5892a847be0efaa'
