98b7cdb83cb05a8119bba330d4584c861e90b575
DeathBorn <228085+DeathBorn@users.noreply.github.com>
2020-09-24 12:09:03 +0300

    add c02b7d5 release support

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,16 +8,16 @@ default['vitess']['topo_global_server_address'] = 'localhost:2379'
 # the topology implementation to use
 default['vitess']['topo_implementation'] = 'etcd2'
 
-default['vitess']['version']['mysqlctld'] = 'v7.0.2-40ee160'
-default['vitess']['version']['vtctlclient'] = 'v7.0.2-40ee160'
-default['vitess']['version']['vtctld'] = 'v7.0.2-40ee160'
-default['vitess']['version']['vtgate'] = 'v7.0.2-40ee160'
-default['vitess']['version']['vttablet'] = 'v7.0.2-40ee160'
-default['vitess']['version']['vtworker'] = 'v7.0.2-40ee160'
-default['vitess']['version']['mysqlctl'] = 'v7.0.2-40ee160'
-default['vitess']['version']['vtctl'] = 'v7.0.2-40ee160'
-default['vitess']['version']['vtexplain'] = 'v7.0.2-40ee160'
-default['vitess']['version']['vtbench'] = 'v7.0.2-40ee160'
+default['vitess']['version']['mysqlctld'] = 'v8.0-dev-c02b7d5'
+default['vitess']['version']['vtctlclient'] = 'v8.0-dev-c02b7d5'
+default['vitess']['version']['vtctld'] = 'v8.0-dev-c02b7d5'
+default['vitess']['version']['vtgate'] = 'v8.0-dev-c02b7d5'
+default['vitess']['version']['vttablet'] = 'v8.0-dev-c02b7d5'
+default['vitess']['version']['vtworker'] = 'v8.0-dev-c02b7d5'
+default['vitess']['version']['mysqlctl'] = 'v8.0-dev-c02b7d5'
+default['vitess']['version']['vtctl'] = 'v8.0-dev-c02b7d5'
+default['vitess']['version']['vtexplain'] = 'v8.0-dev-c02b7d5'
+default['vitess']['version']['vtbench'] = 'v8.0-dev-c02b7d5'
 
 # host to send spans to. if empty, no tracing will be done
 default['vitess']['datadog-agent-host'] = nil
